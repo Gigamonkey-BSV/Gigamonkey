33abf7a67a988e856ac3a87306349350f287c833
Katrina Knight <kat.knight@newgaea.net>
2022-11-05 13:59:00 -0400

    Added a constexpr string

---
+++ src/gigamonkey/p2p/header_string.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 Katrina Knight
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+
+#include <gigamonkey/p2p/header_string.hpp>
+
+namespace Gigamonkey::Bitcoin {
+
+
+	
+}
