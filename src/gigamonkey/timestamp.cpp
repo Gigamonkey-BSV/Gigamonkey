3937a7800b097f69df8ef7031643eabeed6b3494
Daniel Krawisz <danielkrawisz@protonmail.com>
2023-02-24 22:51:18 -0500

    fix mistake with the way that timestamp was displayed

--- src/gigamonkey/timestamp.cpp
+++ src/gigamonkey/timestamp.cpp
@@ -8,9 +8,9 @@ namespace Gigamonkey::Bitcoin {
     std::ostream& operator<<(std::ostream& o, const timestamp& s) {
         time_t t = static_cast<time_t>(uint32(s));
         std::stringstream ss;
-        ss << "{" << uint32(s) << ", " << ctime(&t);
+        ss << "{" << uint32(s) << ", \"" << ctime(&t);
         string str = ss.str();
-        return o << "\"" << str.substr(0, str.size() - 1) << "\"";
+        return o << str.substr(0, str.size() - 1) << "\"}";
     }
     
 }
