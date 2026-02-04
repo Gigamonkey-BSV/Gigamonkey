afedbd30fb5f6fabbe9a221a89e7163763ad5fbc
Daniel Krawisz <danielkrawisz@protonmail.com>
2026-02-04 15:36:55 -0500

    In data, we started using uint big endian for hash digests. Little endian is actually very inconvenient if you don't require it. We now have our own versions of hash functions here that use little endian digests.

--- src/gigamonkey/include/gigamonkey/hash.hpp
+++ src/gigamonkey/include/gigamonkey/hash.hpp
@@ -10,8 +10,14 @@
 namespace Gigamonkey {
     namespace crypto = data::crypto;
 
-    template <size_t size>
-    using digest = data::hash::digest<size>;
+    // digests are little endian numbers. That's a Bitcoin thing.
+    // Therefore if you write them out as numbers they are byte
+    // reversed from how they really are.
+    template <size_t s> struct digest : public uint<s> {
+        using uint<s>::uint;
+        digest (const uint<s> &u) : uint<s> {u} {}
+        bool valid () const;
+    };
 
     // because of a bug in bitcoind long ago, many bitcoin
     // applications expect hashes to be provided backwards.
@@ -41,46 +47,50 @@ namespace Gigamonkey {
     using digest512 = digest<64>;
 
     // supported hash functions.
-    digest160 SHA1 (slice<const byte>);
+    digest160 SHA1 (byte_slice);
     digest160 SHA1 (string_view);
 
-    digest256 SHA2_256 (slice<const byte>);
+    digest256 SHA2_256 (byte_slice);
     digest256 SHA2_256 (string_view);
 
-    digest160 RIPEMD_160 (slice<const byte>);
+    digest160 RIPEMD_160 (byte_slice);
     digest160 RIPEMD_160 (string_view);
 
-    digest256 double_SHA2_256 (slice<const byte> b);
+    digest256 double_SHA2_256 (byte_slice b);
+
+    // Use this for any other hash function (see data::crypto::hash)
+    template <data::hash::Engine e> digest<e::DigestSize> hash (byte_slice b);
+    template <data::hash::Engine e> digest<e::DigestSize> hash (string_view b);
 }
     
 namespace Gigamonkey::Bitcoin {
     
     // bitcoin hash functions.
-    digest160 inline Hash160 (slice<const byte> b) {
-        return crypto::RIPEMD_160 (SHA2_256 (b));
+    digest160 inline Hash160 (byte_slice b) {
+        return RIPEMD_160 (SHA2_256 (b));
     }
 
-    digest256 inline Hash256 (slice<const byte> b) {
+    digest256 inline Hash256 (byte_slice b) {
         return double_SHA2_256 (b);
     }
 
     digest160 Hash160 (string_view b);
     digest256 Hash256 (string_view b);
         
-    digest160 inline address_hash (slice<const byte> b) {
+    digest160 inline address_hash (byte_slice b) {
         return Hash160 (b);
     }
         
-    digest256 inline signature_hash (slice<const byte> b) {
+    digest256 inline signature_hash (byte_slice b) {
         return Hash256 (b);
     }
 
     digest160 inline Hash160 (string_view b) {
-        return Hash160 (slice<const byte> {(const byte*) (b.data ()), b.size ()});
+        return Hash160 (byte_slice {(const byte*) (b.data ()), b.size ()});
     }
 
     digest256 inline Hash256 (string_view b) {
-        return Hash256 (slice<const byte> {(const byte*) (b.data ()), b.size ()});
+        return Hash256 (byte_slice {(const byte*) (b.data ()), b.size ()});
     }
 
 }
@@ -90,19 +100,100 @@ namespace Gigamonkey::Bitcoin {
 // document to the hash writer incrementally and the hash will be
 // calculated as we go along.
 namespace Gigamonkey {
-    using SHA_2_256_writer = data::hash::writer<crypto::hash::SHA2<32>>;
-    using RIPEMD_160_writer = data::hash::writer<crypto::hash::RIPEMD<20>>;
+
+    // given an engine, construct a writer.
+    template <data::hash::Engine engine> struct hash_writer : data::writer<byte> {
+        using digest = Gigamonkey::digest<engine::DigestSize>;
+        hash_writer (digest &d) noexcept: Digest {d}, Hash {} {}
+
+        void write (const byte *b, size_t bytes) noexcept final override {
+            Hash.Update (b, bytes);
+        }
+
+        ~hash_writer () {
+            Hash.Final (Digest.data ());
+        }
+
+        // non copyable
+        hash_writer (const hash_writer &) = delete;
+        hash_writer &operator = (const hash_writer &) = delete;
+        hash_writer (hash_writer &&) = delete;
+        hash_writer &operator = (hash_writer &&) = delete;
+
+    private:
+        digest &Digest;
+        engine Hash;
+    };
+
+    using SHA1_writer = hash_writer<crypto::hash::SHA1>;
+    using SHA2_256_writer = hash_writer<crypto::hash::SHA2<32>>;
+    using RIPEMD_160_writer = hash_writer<crypto::hash::RIPEMD<20>>;
 }
 
 namespace Gigamonkey::Bitcoin {
-    using Hash160_writer = data::hash::writer<crypto::hash::Bitcoin<20>>;
-    using Hash256_writer = data::hash::writer<crypto::hash::Bitcoin<32>>;
+    using Hash160_writer = hash_writer<crypto::hash::Bitcoin<20>>;
+    using Hash256_writer = hash_writer<crypto::hash::Bitcoin<32>>;
 }
 
 namespace Gigamonkey {
 
-    digest256 inline double_SHA2_256 (slice<const byte> b) {
-        return crypto::SHA2_256 (SHA2_256 (b));
+    template <data::hash::Engine e> digest<e::DigestSize> inline hash (byte_slice b) {
+        digest<e::DigestSize> d;
+        hash_writer<e> {d}.write (b.data (), b.size ());
+        return d;
+    }
+
+    template <data::hash::Engine e> digest<e::DigestSize> inline hash (string_view b) {
+        digest<e::DigestSize> d;
+        hash_writer<e> {d}.write ((const byte *) b.data (), b.size ());
+        return d;
+    }
+
+    digest256 inline double_SHA2_256 (byte_slice b) {
+        digest256 d;
+        Bitcoin::Hash256_writer {d}.write (b.data (), b.size ());
+        return d;
+    }
+
+    digest160 inline RIPEMD_160 (byte_slice b) {
+        digest160 d;
+        RIPEMD_160_writer {d}.write (b.data (), b.size ());
+        return d;
+    }
+
+    digest256 inline SHA2_256 (byte_slice b) {
+        digest256 d;
+        SHA2_256_writer {d}.write (b.data (), b.size ());
+        return d;
+    }
+
+    digest160 inline SHA1 (byte_slice b) {
+        digest160 d;
+        SHA1_writer {d}.write (b.data (), b.size ());
+        return d;
+    }
+
+    digest160 inline SHA1 (string_view b) {
+        digest160 d;
+        SHA1_writer {d}.write ((const byte*) (b.data ()), b.size ());
+        return d;
+    }
+
+    digest256 inline SHA2_256 (string_view b) {
+        digest256 d;
+        SHA2_256_writer {d}.write ((const byte*) (b.data ()), b.size ());
+        return d;
+    }
+
+    digest160 inline RIPEMD_160 (string_view b) {
+        digest160 d;
+        RIPEMD_160_writer {d}.write ((const byte*) (b.data ()), b.size ());
+        return d;
+    }
+
+    template<size_t s>
+    bool inline digest<s>::valid () const {
+        return *this != digest {0};
     }
 
     template <size_t size>
@@ -114,30 +205,6 @@ namespace Gigamonkey {
     reader inline &operator >> (reader &r, digest<size> &s) {
         r.read (s.data (), size);
         return r;
-    }
-
-    digest160 inline SHA1 (string_view b) {
-        return SHA1 (slice<const byte> {(const byte*) (b.data ()), b.size ()});
-    }
-
-    digest256 inline SHA2_256 (string_view b) {
-        return SHA2_256 (slice<const byte> {(const byte*) (b.data ()), b.size ()});
-    }
-
-    digest160 inline RIPEMD_160 (string_view b) {
-        return RIPEMD_160 (slice<const byte> {(const byte*) (b.data ()), b.size ()});
-    }
-
-    digest160 inline RIPEMD_160 (slice<const byte> b) {
-        return crypto::RIPEMD_160 (b);
-    }
-
-    digest256 inline SHA2_256 (slice<const byte> b) {
-        return crypto::SHA2_256 (b);
-    }
-
-    digest160 inline SHA1 (slice<const byte> b) {
-        return crypto::SHA1 (b);
     }
     
 }
