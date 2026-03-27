647086bc770a03fed944b51c627f90a1f71aa3b9
Daniel Krawisz <danielkrawisz@protonmail.com>
2026-03-27 12:40:01 -0500

    Tests for REQUIRE_COMPRESSED_PUBKEYTYPE

--- src/gigamonkey/include/gigamonkey/script/config.hpp
+++ src/gigamonkey/include/gigamonkey/script/config.hpp
@@ -22,7 +22,7 @@ namespace Gigamonkey::Bitcoin {
         VERIFY_NONE = 0,
 
         // Evaluate P2SH subscripts (softfork safe, BIP16).
-        // ignored after Genesis.
+        // outputs after genesis are not recognized as P2SH.
         VERIFY_P2SH = (1U << 0),
 
         // Passing a non-strict-DER signature or one with undefined hashtype to a
@@ -38,14 +38,17 @@ namespace Gigamonkey::Bitcoin {
         // Passing a non-strict-DER signature or one with S > order/2 to a checksig
         // operation causes script failure
         // (softfork safe, BIP62 rule 5).
+        // no longer required in script version 2.
         VERIFY_LOW_S = (1U << 3),
 
         // verify dummy stack item consumed by CHECKMULTISIG is of zero-length
         // (softfork safe, BIP62 rule 7).
+        // no longer required in script version 2.
         VERIFY_NULLDUMMY = (1U << 4),
 
         // Using a non-push operator in the scriptSig causes script failure
         // (softfork safe, BIP62 rule 2).
+        // no longer required in script version 2.
         VERIFY_SIGPUSHONLY = (1U << 5),
 
         // Require minimal encodings for all push operations (OP_0... OP_16,
@@ -55,7 +58,7 @@ namespace Gigamonkey::Bitcoin {
         // stack element is interpreted as a number, it must be of minimal length
         // (BIP62 rule 4).
         // (softfork safe)
-        // ignored after Chronicle.
+        // no longer required in script version 2.
         VERIFY_MINIMALDATA = (1U << 6),
 
         // Discourage use of NOPs reserved for upgrades (NOP1-10)
@@ -74,36 +77,42 @@ namespace Gigamonkey::Bitcoin {
         // one stack element must remain, and when interpreted as a boolean, it must
         // be true".
         // (softfork safe, BIP62 rule 6)
+        //
         // Note: CLEANSTACK should never be used without P2SH or WITNESS.
-        // ignored after Chronicle
+        //
+        // no longer required in script version 2.
         VERIFY_CLEANSTACK = (1U << 8),
 
         // Verify CHECKLOCKTIMEVERIFY
         //
         // See BIP65 for details.
+        // disabled after genesis
         VERIFY_CHECKLOCKTIMEVERIFY = (1U << 9),
 
         // support CHECKSEQUENCEVERIFY opcode
         //
         // See BIP112 for details
+        // disabled after genesis.
         VERIFY_CHECKSEQUENCEVERIFY = (1U << 10),
 
         // Require the argument of OP_IF/NOTIF to be exactly 0x01 or empty vector
-        //
+        // no longer required in script version 2.
         VERIFY_MINIMALIF = (1U << 13),
 
         // Signature(s) must be empty vector if an CHECK(MULTI)SIG operation failed
-        //
+        // no longer required in script version 2.
         VERIFY_NULLFAIL = (1U << 14),
 
         // Public keys in scripts must be compressed
         //
+        // no longer required in script version 2.
         VERIFY_COMPRESSED_PUBKEYTYPE = (1U << 15),
 
         // Do we accept signature using SIGHASH_FORKID
         ENABLE_SIGHASH_FORKID = (1U << 16),
 
         // is forkid required?
+        // no longer required after chronicle.
         REQUIRE_SIGHASH_FORKID = (1U << 17),
 
         // OP_RETURN on its own is no longer a valid script.
