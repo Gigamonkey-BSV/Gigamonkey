495c8b09c79103ef11a12bcf15bb1fed5a0ed636
Daniel Krawisz <danielkrawisz@protonmail.com>
2026-06-06 11:43:04 -0500

    Update dockerfile and FindGMP.cmake

--- cmake/FindGMP.cmake
+++ cmake/FindGMP.cmake
@@ -6,84 +6,84 @@
 #  GMP_LIBRARIES         - Link these to use GMP
 
 
-function(define_imported_target library headers)
-  add_library(GMP::GMP UNKNOWN IMPORTED)
-  set_target_properties(GMP::GMP PROPERTIES
+function (define_imported_target library headers)
+  #if we already have GMP, skip the function.
+  if (TARGET GMP::GMP)
+    return ()
+  endif ()
+
+  add_library (GMP::GMP UNKNOWN IMPORTED)
+  set_target_properties (GMP::GMP PROPERTIES
     IMPORTED_LOCATION ${library}
-    INTERFACE_INCLUDE_DIRECTORIES ${headers}
-  )
-  set(GMP_FOUND 1 CACHE INTERNAL "GMP found" FORCE)
-  set(GMP_LIBRARIES ${library}
+    INTERFACE_INCLUDE_DIRECTORIES ${headers})
+
+  set (GMP_FOUND 1 CACHE INTERNAL "GMP found" FORCE)
+  set (GMP_LIBRARIES ${library}
       CACHE STRING "Path to gmp library" FORCE)
-  set(GMP_INCLUDES ${headers}
+  set (GMP_INCLUDES ${headers}
       CACHE STRING "Path to gmp headers" FORCE)
-  mark_as_advanced(FORCE GMP_LIBRARIES)
-  mark_as_advanced(FORCE GMP_INCLUDES)
-endfunction()
+  mark_as_advanced (FORCE GMP_LIBRARIES)
+  mark_as_advanced (FORCE GMP_INCLUDES)
+endfunction ()
 
 # Accepting user-provided paths and reusing cached values
 if (GMP_LIBRARIES AND GMP_INCLUDES)
-  define_imported_target(${GMP_LIBRARIES} ${GMP_INCLUDES})
-  return()
-endif()
+  define_imported_target (${GMP_LIBRARIES} ${GMP_INCLUDES})
+  return ()
+endif ()
 
-set(QUIET_ARG)
-if(GMP_FIND_QUIETLY)
-  set(QUIET_ARG QUIET)
-endif()
+set (QUIET_ARG)
+if (GMP_FIND_QUIETLY)
+  set (QUIET_ARG QUIET)
+endif ()
 
-set(REQUIRED_ARG)
-if(GMP_FIND_REQUIRED)
-  set(REQUIRED_ARG REQUIRED)
-endif()
+set (REQUIRED_ARG)
+if (GMP_FIND_REQUIRED)
+  set (REQUIRED_ARG REQUIRED)
+endif ()
 
-file(TO_CMAKE_PATH "$ENV{GMP_DIR}" _GMP_DIR)
+file (TO_CMAKE_PATH "$ENV{GMP_DIR}" _GMP_DIR)
 
-include(FindPackageHandleStandardArgs)
+include (FindPackageHandleStandardArgs)
 
-  find_path(GMP_INCLUDE_DIR
-            NAMES gmp.h
-            HINTS ENV GMP_INC_DIR
-                  ENV GMP_DIR
-                  $ENV{GMP_DIR}/include
-            PATH_SUFFIXES include
-  	        DOC "The directory containing the GMP header files"
-           )
+  find_path (GMP_INCLUDE_DIR
+    NAMES gmp.h gmpxx.h
+    HINTS ENV GMP_INC_DIR
+      ENV GMP_DIR
+      $ENV{GMP_DIR}/include
+    PATH_SUFFIXES include
+    DOC "The directory containing the GMP header files")
 
-  find_library(GMP_LIBRARY_RELEASE NAMES gmp libgmp-10 gmp-10 mpir
+  find_library (GMP_LIBRARY_RELEASE NAMES gmp libgmp-10 gmp-10 mpir
     HINTS ENV GMP_LIB_DIR
           ENV GMP_DIR
           $ENV{GMP_DIR}/lib
     PATH_SUFFIXES lib
-    DOC "Path to the Release GMP library"
-    )
+    DOC "Path to the Release GMP library")
 
-  find_library(GMP_LIBRARY_DEBUG NAMES gmpd gmp libgmp-10 gmp-10 mpir
+  find_library (GMP_LIBRARY_DEBUG NAMES gmpd gmp libgmp-10 gmp-10 mpir
     HINTS ENV GMP_LIB_DIR
           ENV GMP_DIR
           $ENV{GMP_DIR}/include
     PATH_SUFFIXES lib
-    DOC "Path to the Debug GMP library"
-    )
+    DOC "Path to the Debug GMP library")
 
-  
-    if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
-      set(GMP_LIBRARIES ${GMP_LIBRARY_DEBUG})
-    else()
+    if ("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
+      set (GMP_LIBRARIES ${GMP_LIBRARY_DEBUG})
+    else ()
       set(GMP_LIBRARIES ${GMP_LIBRARY_RELEASE})
-    endif()
+    endif ()
 
   # Attempt to load a user-defined configuration for GMP if couldn't be found
   if ( NOT GMP_INCLUDE_DIR OR NOT GMP_LIBRARIES)
-    include( GMPConfig OPTIONAL )
-  endif()
+    include ( GMPConfig OPTIONAL )
+  endif ()
 
 find_package_handle_standard_args(GMP DEFAULT_MSG GMP_LIBRARIES GMP_INCLUDE_DIR)
 if (GMP_FOUND)
   define_imported_target(
     "${GMP_LIBRARIES}"
-    "${GMP_INCLUDE_DIR}"
-  )
-elseif(GMP_FIND_REQUIRED)
-  message(FATAL_ERROR "Required Gmp library not found")
-endif()
+    "${GMP_INCLUDE_DIR}")
+elseif (GMP_FIND_REQUIRED)
+  message (FATAL_ERROR "Required Gmp library not found")
+endif ()
